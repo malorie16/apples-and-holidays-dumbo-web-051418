ddb07fee85d6184c9ee3f48692c7a5f6e9348b35
malorie16 <malorie18@gmail.com>
2018-05-08 16:09:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,8 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, stuff|
          holiday if holiday_hash[season][holiday].include?("BBQ")
-       elsif holiday_hash[season][holiday].delete_if do |holiday, stuff|
-         holiday.include?("nil")
+       else puts ""
        end
 
     end
