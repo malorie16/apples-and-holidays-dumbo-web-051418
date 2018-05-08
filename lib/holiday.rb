44323144baaa72c14772f34883045eb00fe1263c
malorie16 <malorie18@gmail.com>
2018-05-08 15:49:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
     holidays.select do |holiday, stuff|
-         stuff.include?(["BBQ"])
-         puts "#{holiday}"
+         holiday if stuff.include?(["BBQ"])
+       
     end
   end
 end
